4cff06fa1db70afde1407f734d2610a800cda999
Jerome Purushotham <jerome.purushotham@gmail.com>
2021-10-13 22:10:55 -0700

    Add proper bootstrap servers (running in docker), and value serializer and deserializer for producer, consumer respectively

--- consumer.py
+++ consumer.py
@@ -1,8 +1,11 @@
 from kafka import KafkaConsumer
+from json import loads
 
 
 TOPIC_NAME = 'items'
 
-consumer = KafkaConsumer(TOPIC_NAME)
+consumer = KafkaConsumer(TOPIC_NAME,
+                         bootstrap_servers=['localhost:29092'],
+                         value_deserializer=lambda x: loads(x.decode('utf-8')))
 for message in consumer:
     print(message)
--- producer.py
+++ producer.py
@@ -1,10 +1,16 @@
 from kafka import KafkaProducer
+from json import dumps
+import time
 
 
 TOPIC_NAME = 'items'
-KAFKA_SERVER = 'localhost:9092'
+KAFKA_SERVER = 'localhost:29092'
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER,
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-producer.send(TOPIC_NAME, b'Test Message!!!')
-producer.flush()
+for x in range(1000):
+    data = {"n": x}
+    producer.send(TOPIC_NAME, value=data)
+    print(f"Sent data ...{x}")
+    time.sleep(5)
